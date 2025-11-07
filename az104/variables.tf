32f744256d56f271ad0598a6dec23ab5f3e3d328
ppfura <136515860+ppfura@users.noreply.github.com>
2025-11-06 23:15:28 -0500

    Update variables.tf

--- az104/variables.tf
+++ az104/variables.tf
@@ -1,1 +1,31 @@
+variable "resource_group_name" {
+  description = "Nombre del grupo de recursos"
+  type        = string
+}
+
+variable "location" {
+  description = "Región de Azure"
+  type        = string
+  default     = "East US"
+}
+
+variable "vnet_name" {
+  description = "Nombre de la VNet"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Espacio de direcciones de la VNet"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Nombre de la Subnet"
+  type        = string
+}
+
+variable "subnet_address_prefix" {
+  description = "Prefijo de direcciones de la Subnet"
+  type        = string
+}
 
